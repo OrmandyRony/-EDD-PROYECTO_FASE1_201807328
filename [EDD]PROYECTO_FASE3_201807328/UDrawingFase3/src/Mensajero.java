28539c93cb674a0e8e5781b488e771e0f5f3bbb8
Rony Ormandy Ortíz Alvarez <ormandyrony@gmail.com>
2022-04-20 23:13:26 -0600

    Clase mensajero creada

---
+++ [EDD]PROYECTO_FASE3_201807328/UDrawingFase3/src/Mensajero.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author orman
+ */
+public class Mensajero {
+    String nombre;
+    String apellido;
+    String tipoLicencia;
+    String genero;
+    String telefono;
+    String dirección;
+
+    public Mensajero(String nombre, String apellido, String tipoLicencia, String genero, String telefono, String dirección) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipoLicencia = tipoLicencia;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.dirección = dirección;
+    }
+    
+    
+}
