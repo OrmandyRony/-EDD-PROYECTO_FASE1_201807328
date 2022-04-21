e2cd707c8cde35b1095e1fa9be85eb9a2777e89c
Rony Ormandy Ortíz Alvarez <ormandyrony@gmail.com>
2022-04-20 23:11:23 -0600

    Clase cliente creada

---
+++ [EDD]PROYECTO_FASE3_201807328/UDrawingFase3/src/Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author orman
+ */
+public class Cliente {
+    String nombre;
+    String usuario;
+    String correo;
+    String DPI;
+    String telefonos;
+    String contrasena;
+    String direccion;
+    String idMunicipio;
+
+    public Cliente(String nombre, String usuario, String correo, String DPI, String telefonos, String contrasena, String direccion, String idMunicipio) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.DPI = DPI;
+        this.telefonos = telefonos;
+        this.contrasena = contrasena;
+        this.direccion = direccion;
+        this.idMunicipio = idMunicipio;
+    }
+    
+    
+     
+}
